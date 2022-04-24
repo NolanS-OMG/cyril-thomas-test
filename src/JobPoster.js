f8b188e2103376614ab82f1927b5be3ff10cd1b4
nolan <nolan1scott3@gmail.com>
2022-04-23 21:38:37 -0500

    Fixed: Misstyping of a function

--- src/JobPoster.js
+++ src/JobPoster.js
@@ -3,7 +3,7 @@ import {useForm} from "react-hook-form";
 
 const JobPoster = () => {
 
-  const {register, handelSubmit} = useForm()
+  const {register, handleSubmit} = useForm()
 
   const [job, setJob] = useState({})
 
@@ -16,8 +16,8 @@ const JobPoster = () => {
   }
 
   return (
-    <form onSubmit={handelSubmit( onSubmit )}>
-      <h4>Basic details</h4>
+    <form onSubmit={handleSubmit( onSubmit )}>
+      <h4>Basic Details</h4>
       <label>Job: </label>
       <input {...register('student', {required: true} ) }/>
       <h4>Targeting</h4>
